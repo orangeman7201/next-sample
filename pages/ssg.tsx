3ddc6ff7a5bc85951f2f26ec95adfe5d75c2d63b
AkitaYuma <yuma.akita@smart-sou.co.jp>
2023-05-06 18:16:21 +0900

    getStaticPropsを使った静的ページを作成

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,9 +1,13 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage, NextPageContext } from 'next'
 import Head from 'next/head'
 
-type SSGProps = {}
+type SSGProps = {
+  message: string
+}
 
-const SSG: NextPage<SSGProps> = () => {
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+
   return (
     <div>
       <Head>
@@ -14,9 +18,25 @@ const SSG: NextPage<SSGProps> = () => {
         <p>
           このページはSSGで生成されました。
         </p>
+        <p>
+          {message}
+        </p>
       </main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp =  new Date().toLocaleString()
+  const message = `${timestamp}に更新されました。`
+
+  console.log(message)
+  console.log(context.locales)
+  return {
+    props: {
+      message
+    }
+  }
 }
 
 export default SSG
